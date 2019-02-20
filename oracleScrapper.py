2e4d11097a68812c8442cf389481cf08751dcf60
Unknown <adhamalsalah1@gmail.com>
2019-02-19 22:33:15 -0500

    DocFetcher V0.5
    This version is the MVP
    
    +Includes an API wrapper for Oracle JDK Documentation for JDK 7, 8, and 11
    
    +Is able to search through comments in the /r/learnjava and the /r/javahelp subreddits
    
    +able to leave comments for users giving it commands
    
    +accepts macros
    
    example of user comment:
    "!DocFetcher java8 Integer"
    
    DocFetcher then looks through the oracle java 8 classes api, finds Integer, then returns a brief description and a link to the Integer class page.

---
+++ oracleScrapper.py
@@ -0,0 +1,89 @@
+import bs4 as bs
+import urllib.request
+
+
+# This function searches the oracle java 11 api for a word a user wants and returns its information
+def _java11_scrapper(j_class):
+    sauce = urllib.request.urlopen('https://docs.oracle.com/en/java/javase/11/docs/api/allclasses.html').read()
+    soup = bs.BeautifulSoup(sauce, 'lxml')
+    string = ""
+    is_found = False
+    for url in soup.find_all('a'):
+        if j_class == url.string:
+            link = "https://docs.oracle.com/en/java/javase/11/docs/api/" + url.get('href')
+            inner_sauce = urllib.request.urlopen(link)
+            broth = bs.BeautifulSoup(inner_sauce, 'lxml')
+            string += ("#here's what I found about the **" + j_class + "** class:\n\n")
+            string += "##Java 11\n\n"
+            string += ("~~~\n" + broth.pre.text + "\n~~~" "\n\n")
+            string += ("\n\n" + broth.find('div', class_='block').text + "\n\n")
+            string += ("[Class Documentation Page](" + link + ")\n\n")
+            is_found = True
+            break
+    if is_found is False:
+        string += "I was not able to find any information about **" + j_class + "** in the oracle documentation.\n\n"
+        string += "[Oracle Java 11 Documentation](https://docs.oracle.com/en/java/javase/11/docs/api/index.html)\n\n"
+
+    string += "^(I am a bot still in development. For questions or suggestions, you can message /u/ADhoom)"
+    return string
+
+
+# This function searches the oracle java 8 api for a word a user wants and returns its information
+def _java8_scrapper(j_class):
+    sauce = urllib.request.urlopen('https://docs.oracle.com/javase/8/docs/api/allclasses-noframe.html').read()
+    soup = bs.BeautifulSoup(sauce, 'lxml')
+    string = ""
+    is_found = False
+    for url in soup.find_all('a'):
+        if j_class == url.string:
+            link = "https://docs.oracle.com/javase/8/docs/api/" + url.get('href')
+            inner_sauce = urllib.request.urlopen(link)
+            broth = bs.BeautifulSoup(inner_sauce, 'lxml')
+            string += ("#here's what I found about the **" + j_class + "** class:\n\n")
+            string += "##Java 8\n\n"
+            string += ("~~~\n" + broth.pre.text + "\n~~~" "\n\n")
+            string += ("\n\n" + broth.find('div', class_='block').text + "\n\n")
+            string += ("[Class Documentation Page](" + link + ")\n\n")
+            is_found = True
+            break
+    if is_found is False:
+        string += "I was not able to find any information about **" + j_class + "** in the oracle documentation.\n\n"
+        string += "[Oracle Java 8 Documentation](https://docs.oracle.com/javase/8/docs/api/overview-summary.html)\n\n"
+
+    string += "^(I am a bot still in development. For questions or suggestions, you can message /u/ADhoom)"
+    return string
+
+
+# This function searches the oracle java 7 api for a word a user wants and returns its information
+def _java7_scrapper(j_class):
+    sauce = urllib.request.urlopen('https://docs.oracle.com/javase/7/docs/api/allclasses-noframe.html').read()
+    soup = bs.BeautifulSoup(sauce, 'lxml')
+    string = ""
+    is_found = False
+    for url in soup.find_all('a'):
+        if j_class == url.string:
+            link = "https://docs.oracle.com/javase/7/docs/api/" + url.get('href')
+            inner_sauce = urllib.request.urlopen(link)
+            broth = bs.BeautifulSoup(inner_sauce, 'lxml')
+            string += ("#here's what I found about the **" + j_class + "** class:\n\n")
+            string += "##Java 7\n\n"
+            string += ("~~~\n" + broth.pre.text + "\n~~~" "\n\n")
+            string += ("\n\n" + broth.find('div', class_='block').text + "\n\n")
+            string += ("[Class Documentation Page](" + link + ")\n\n")
+            is_found = True
+            break
+    if is_found is False:
+        string += "I was not able to find any information about **" + j_class + "** in the oracle documentation.\n\n"
+        string += "[Oracle Java 7 Documentation](https://docs.oracle.com/javase/7/docs/api/overview-summary.html)\n\n"
+
+    string += "^(I am a bot still in development. For questions or suggestions, you can message /u/ADhoom)"
+    return string
+
+
+def search_jdk(version, java_class):
+    if version == "java11" or version == "j11":
+        return _java11_scrapper(java_class)
+    elif version == "java8" or version == "j8" or version == "java":
+        return _java8_scrapper(java_class)
+    elif version == "java7" or version == "j7":
+        return _java7_scrapper(java_class)
---
+++ redditBot.py
@@ -0,0 +1,75 @@
+import praw
+import config
+import time
+from oracleScrapper import *
+
+# This function creates the connection between this script and reddit via PRAW
+def bot_login():
+    print("Logging " + config.username + " in...")
+    credentials = praw.Reddit(username=config.username,
+                              password=config.password,
+                              client_id=config.client_id,
+                              client_secret=config.client_secert,
+                              user_agent="Programming Language Documentation Fetcher Bot v0.5")
+    return credentials
+
+
+# this function takes in all replied to comments from a file and inserts it into a list
+def get_saved_comments():
+    with open("comments_replied_to.txt", "a+") as f:
+        f.seek(0)
+        comments = f.read()
+        comments = comments.split("\n")
+        comments = list(filter(None, comments))
+    return comments
+
+
+# This function reads through reddit comments in a subreddit
+# once a user calls the bot, it looks for keywords such as programming language names and searches for class
+# the typical command should look like this "!DocFetcher <Language> <KeyWord>"
+def run_bot(access, comments):
+    for comment in access.subreddit('learnjava+javahelp').comments(limit=25):
+        time.sleep(3)
+        print("Searching for comments...")
+        if ("!DocFetcher" in comment.body or "!df" in comment.body or "!DF" in comment.body) and comment.id not in comments and not comment.author == access.user.me():
+            print("Found Comment...")
+            if "j11" in comment.body or "java11" in comment.body:
+                com = comment.body.split()
+                if len(com) == 3:
+                    if (com[0] == "!DocFetcher" or com[0] == "!df" or com[0] == "!DF") and ("j" in com[1] and "11" in com[1]):
+                        # search_jdk is a method inside oracleScrapper
+                        reply = search_jdk(com[1], com[-1])
+                        print("Leaving Reply for " + comment.id + "...")
+                        comment.reply(reply)
+                        comments.append(comment.id)
+                        # append the comment ID to the comments file so as not to spam comments already replied to
+                        with open("comments_replied_to.txt", "a") as f:
+                            f.write(comment.id + "\n")
+            elif "java7" in comment.body or "j7" in comment.body:
+                com = comment.body.split()
+                if len(com) == 3:
+                    if (com[0] == "!DocFetcher" or com[0] == "!df" or com[0] == "!DF") and ("j" in com[1] and "7" in com[1]):
+                        reply = search_jdk(com[1], com[-1])
+                        print("Leaving Reply for " + comment.id + "...")
+                        comment.reply(reply)
+                        comments.append(comment.id)
+                        with open("comments_replied_to.txt", "a") as f:
+                            f.write(comment.id + "\n")
+            elif "java8" in comment.body or "j8" in comment.body or "java" in comment.body:
+                com = comment.body.split()
+                if len(com) == 3:
+                    if (com[0] == "!DocFetcher" or com[0] == "!df" or com[0] == "!DF") and (com[1] == "j8" or com[1] == "java" or com[1] == "java8"):
+                        reply = search_jdk(com[1], com[-1])
+                        print("Leaving Reply for " + comment.id + "...")
+                        comment.reply(reply)
+                        comments.append(comment.id)
+                        with open("comments_replied_to.txt", "a") as f:
+                            f.write(comment.id + "\n")
+    print("sleeping...")
+    time.sleep(60)
+
+
+while True:
+    comments_replied = get_saved_comments()
+    login = bot_login()
+    run_bot(login, comments_replied)
